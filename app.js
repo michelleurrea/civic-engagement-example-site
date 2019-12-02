099847e112ce8a478a507071288a1525946f1c89
michelleurrea <michelleurrea@gmail.com>
2019-12-02 06:40:48 -0800

    Started playing around with dom manipulation within JS file

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("Hello world!")
+
+var myStaff = document.getElementsByClassName("employee")
+
+console.log(myStaff[0].innerText);
+console.log(myStaff[1]);
+console.log(myStaff[2]);
+console.log(myStaff[3]);
+console.log(myStaff[4]);
+console.log(myStaff[5]);
+
+var myStaffTag = document.getElementsByTagName("a")
+
+console.log(myStaffTag[0].innerText);
+console.log(myStaffTag[1].innerText);
+console.log(myStaffTag[2]);
+console.log(myStaffTag[3]);
+console.log(myStaffTag[4]);
+console.log(myStaffTag[5]);
